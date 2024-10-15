1c04bacbd29f62fa94ae341e85e61de8442299eb
mrzxd69 <mrzxd69@gmail.com>
2024-10-15 17:20:50 +0600

    feat: format lesson line

--- src/shared/utils/schedule.ts
+++ src/shared/utils/schedule.ts
@@ -21,8 +21,8 @@ export const processingLesson = async (text: string, lesson: any, isTeacher: boo
     const teacherText = !isTeacher && teacherName ? `\n   Ведёт: <i>${teacherName}</i>` : "";
 
     if (lesson.status === LessonStatus.Joined) text += `\n<b>${count === 0 ? "Промежуточ." : "Пара: " + count}</b>\n  ${descipline}\n   Кабинет: <b>${room}</b>${teacherText}\n`;
-    if (lesson.status === LessonStatus.SubGroup1) text += `\n<b>Пара: ${count}</b>\n  ${descipline}\n   [1] - <b>${room}</b> | ${teacherText.replace("\n   Ведёт: ", "")}\n`;
-    if (lesson.status === LessonStatus.SubGroup2) text += `   [2] - <b>${room}</b> | ${teacherText.replace("\n   Ведёт: ", "")}\n`;
+    if (lesson.status === LessonStatus.SubGroup1) text += `\n<b>Пара: ${count}</b>\n  ${descipline}\n   [1] - <b>${room}</b>${teacherText.replace("\n   Ведёт: ", " | ")}\n`;
+    if (lesson.status === LessonStatus.SubGroup2) text += `   [2] - <b>${room}</b>${teacherText.replace("\n   Ведёт: ", " | ")}\n`;
 
 
     return text;
