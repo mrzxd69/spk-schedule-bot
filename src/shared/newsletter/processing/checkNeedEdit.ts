d6df4a2323f2b78cbdd7dc6bc0b01cb90b646f4a
mrzxd69 <mrzxd69@gmail.com>
2024-10-16 21:22:52 +0600

    fix: all received schedule

--- src/shared/newsletter/processing/checkNeedEdit.ts
+++ src/shared/newsletter/processing/checkNeedEdit.ts
@@ -3,97 +3,67 @@ import { prisma } from "@postgresql/prisma";
 export const checkNeedEdit = async (group: string, lesson: string, lessonValue: any[]) => {
 	let needEdit = false;
 
-	for (const lesson in lessonValue) {
-		if (lessonValue[lesson].length === 1) {
-			if (lessonValue[lesson][0].length === 3) {
+	for (const count in lessonValue) {
+		if (lessonValue[count].length === 1) {
+			if (lessonValue[count][0].length === 3) {
 				const existLesson = await prisma.lessons.findFirst({
 					where: {
-						AND: [
-							{ group },
-							{ count: Number(lesson) },
-							{
-								descipline: lessonValue[lesson][0][0],
-							},
-							{
-								room: String(lessonValue[lesson][0][2]),
-							},
-							{
-								status: Number(lessonValue[lesson][0][1]) === 1 ? "SubGroup1" : "SubGroup2",
-							},
-						],
+						group,
+						count: Number(count),
+						status: Number(lessonValue[count][0][1]) === 1 ? "SubGroup1" : "SubGroup2",
+						room: String(lessonValue[count][0][2])
 					},
 				});
 
 				if (!existLesson) {
+					console.log(1)
 					needEdit = true;
 				}
 			}
 		}
 
-		if (lessonValue[lesson].length === 1) {
-			if (lessonValue[lesson][0].length === 2) {
+		if (lessonValue[count].length === 1) {
+			if (lesson[0].length === 2) {
+
 				const existLesson = await prisma.lessons.findFirst({
 					where: {
-						AND: [
-							{ group },
-							{ count: Number(lesson) },
-							{
-								descipline: lessonValue[lesson][0][0],
-							},
-							{
-								room: String(lessonValue[lesson][0][1]),
-							},
-							{
-								status: "JOINED",
-							},
-						],
+						status: "JOINED",
+						room: String(lessonValue[count][0][1]),
+						descipline: lessonValue[count][0][0],
+						count: Number(count)
 					},
 				});
 
 				if (!existLesson) {
+					console.log(2)
 					needEdit = true;
 				}
 			}
 		}
 
-		if (lessonValue[lesson].length === 2) {
+		if (lessonValue[count].length === 2) {
 			const existLesson1 = await prisma.lessons.findFirst({
 				where: {
-					AND: [
-						{ group },
-						{ count: Number(lesson) },
-						{
-							descipline: lessonValue[lesson][0][0],
-						},
-						{
-							room: String(lessonValue[lesson][0][2]),
-						},
-						{
-							status: lessonValue[lesson][0][1] === 1 ? "SubGroup1" : "SubGroup2",
-						},
-					],
+					group,
+					count: Number(count),
+					descipline: lessonValue[count][0][0],
+					room: String(lessonValue[count][0][2]),
+					status: lessonValue[count][0][1] == 1 ? "SubGroup1" : "SubGroup2"
 				},
 			});
 
 			const existLesson2 = await prisma.lessons.findFirst({
 				where: {
-					AND: [
-						{ group },
-						{ count: Number(lesson) },
-						{
-							descipline: lessonValue[lesson][1][0],
-						},
-						{
-							room: String(lessonValue[lesson][1][2]),
-						},
-						{
-							status: lessonValue[lesson][1][1] === 1 ? "SubGroup1" : "SubGroup2",
-						},
-					],
+					group,
+					count: Number(count),
+					descipline: lessonValue[count][1][0],
+					room: String(lessonValue[count][1][2]),
+					status: lessonValue[count][1][1] == 1 ? "SubGroup1" : "SubGroup2"
 				},
 			});
 
 			if (!existLesson1 || !existLesson2) {
+				console.log(3)
 				needEdit = true;
 			}
 		}
@@ -101,5 +71,5 @@ export const checkNeedEdit = async (group: string, lesson: string, lessonValue: 
 
 	return {
 		needEdit,
-	};
-};
+	}
+}
